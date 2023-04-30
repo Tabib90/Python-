e8d67a3cc703bce61288e6fb2c1746c2f40229c4
Fatin Tabib <106029142+Tabib90@users.noreply.github.com>
2023-04-30 20:39:22 +0600

    Add files via upload

---
+++ print.py
@@ -0,0 +1,12 @@
+# PRINT FUNCTION IN PYTHON
+
+# Print 
+
+print("Hello","How are you",sep=",",end=".") # the sep function put a comma between the two lines and the end function put a dot at the end of the line
+
+#print from user input
+
+a=input("enter your number:")
+print("the number is:",a)
+
+#Thanks
