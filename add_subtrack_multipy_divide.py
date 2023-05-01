368419fe21a8c5a44ee97864f436b179c1848dea
Fatin Tabib <106029142+Tabib90@users.noreply.github.com>
2023-05-01 10:53:03 +0600

    Add files via upload

---
+++ add_subtrack_multipy_divide.py
@@ -0,0 +1,36 @@
+
+# ADDITION - SUBSTITUTION - MULTIPLYCATION - DIVISON IN PYTHON
+
+# Declear two user input
+
+a=int(input("Enter 1st number:")) # here we declear two user input which is integer type
+b=int(input("Enter 2nd number:"))
+
+print("\n") # for a new line
+print("\n")
+# Addition
+
+
+sum=a+b
+print("The sum is:",sum)
+
+#Substitution
+
+sub = a-b
+print("The substitution is:",sub)
+
+#Multiplycation
+
+mul = a*b
+print("The multiply of two number is:",mul)
+
+#Divison
+
+div = a/b
+print("The divison of two number is:",div)
+
+print("\n")
+print("\n")
+print("\n")
+
+# Thanks
